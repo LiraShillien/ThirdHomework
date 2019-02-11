28885b5301cc486ad8a5c09fac6c2d7499820167
Lira <liramail96@gmail.com>
2019-02-11 14:29:51 +0200

    bubble sort

--- BubbleSort.php
+++ BubbleSort.php
@@ -4,12 +4,13 @@ function bubbleSort(&$a)
     for($i = 1; $i < count($a)-1; $i++){
         for($j = count($a)-1; $j >= $i; $j--){
             if($a[$j] < $a[$j-1]){
-                list($a[$j], $a[$j-1]) = array($a[$j-1], $a[$j]);
+                $temp = $a[$j];
+                $a[$j] = $a[$j-1];
+                $a[$j-1] = $temp;
             }
         }
     }
     return($a);
 }
-
 $a = [66,33,5,44,88,1,6];
 var_export(bubbleSort($a));
