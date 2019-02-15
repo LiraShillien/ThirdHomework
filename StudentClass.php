52eef0b84480b6a17c3026ddde0e0b4887f493d0
Lira <liramail96@gmail.com>
2019-02-15 11:41:55 +0200

    student class

---
+++ StudentClass.php
@@ -0,0 +1,109 @@
+<?php
+
+class Student
+{
+    public $firstname;
+    public $lastname;
+    public $gender;
+    public $status;
+    public $gpa;
+
+    public function showMyself()
+    {
+        echo $this->firstname . PHP_EOL, $this->lastname . PHP_EOL,
+            $this->isValidGender() . PHP_EOL, $this->status . PHP_EOL,
+            $this->isValidGpa() . PHP_EOL;
+
+    }
+
+    public function studyTime($studyTime)
+    {
+        $this->gpa += log($studyTime);
+    }
+
+    private function isValidGender()
+    {
+        return ($this->gender == 'male' || $this->gender == 'female')
+            ? $this->gender
+            : 'Error: Gender can be equal to \'male\' or \'female\'';
+    }
+
+    private function isValidGpa()
+    {
+        return ($this->gpa > 4)
+            ? 4
+            : $this->gpa;
+
+    }
+
+    public function setValues($value)
+    {
+        $this->firstname = $value['firstname'];
+        $this->lastname = $value['lastname'];
+        $this->gender = $value['gender'];
+        $this->status = $value['status'];
+        $this->gpa = $value['gpa'];
+    }
+}
+
+
+$students = [
+    [
+        'firstname' => 'Mike',
+        'lastname' => 'Barnes',
+        'gender' => 'male',
+        'status' => 'freshman',
+        'gpa' => '4'
+    ],
+    [
+        'firstname' => 'Jim',
+        'lastname' => 'Nickerson',
+        'gender' => 'male',
+        'status' => 'sophomore',
+        'gpa' => '3'
+    ],
+    [
+        'firstname' => 'Jack',
+        'lastname' => 'Indabox',
+        'gender' => 'male',
+        'status' => 'junior',
+        'gpa' => '2.5'
+    ],
+    [
+        'firstname' => 'Jane',
+        'lastname' => 'Miller',
+        'gender' => 'female',
+        'status' => 'senior',
+        'gpa' => '3.6'
+    ],
+    [
+        'firstname' => 'Mary',
+        'lastname' => 'Scott',
+        'gender' => 'female',
+        'status' => 'senior',
+        'gpa' => '2.7'
+    ],
+];
+
+
+
+function objectsCreation($students)
+{
+    $studentList = [];
+    foreach ($students as $key => $value) {
+        $studentList[$key] = new Student();
+        $studentList[$key]->setValues($value);
+    }
+    return $studentList;
+}
+
+
+function objectsShowing($array)
+{
+    foreach ($array as $key=>$value){
+        $value->showMyself();
+        echo PHP_EOL;
+    }
+}
+
+objectsShowing(objectsCreation($students));
