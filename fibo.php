7f1a861857abe0960e8ab70130536364b15083a7
Lira <liramail96@gmail.com>
2019-02-23 14:36:00 +0200

    fibo

--- fibo.php
+++ fibo.php
@@ -3,13 +3,11 @@
 function fibo($x)
 {
     $arr = [1,1];
-
-    for($i=0; $i<INF;$i++){
-            $arr[] = $arr[$i] + $arr[$i + 1];
-            if(array_key_exists($x,$arr)){
-                break;
-            }
+    $i = 0;
+    while(!array_key_exists($x,$arr)){
+        $arr[] = $arr[$i] + $arr[$i+1];
+        $i++;
     }
     return $arr[$x];
 }
-echo fibo(0);
+echo fibo(8);
